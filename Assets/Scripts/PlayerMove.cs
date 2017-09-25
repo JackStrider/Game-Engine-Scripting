0551e343170e5091ce4cc1ead116879b4d541d3f
William Rossi <William.Rossi@iam.local>
2017-09-25 15:16:14 -0500

    Refactored movement and jump to use physics system

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,50 +4,96 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour {
 
-	// Use this for initialization
-	void Start ()
-    {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        //if (Input.GetKey(KeyCode.D))
-        //{
-        //    transform.Translate(0.1f, 0, 0);
-        //}
-        //if (Input.GetKey(KeyCode.A))
-        //{
-        //    transform.Translate(-0.1f, 0, 0);
-        //}
-        //if (Input.GetKey(KeyCode.LeftArrow))
-        //{
-        //    transform.Translate(-0.1f, 0, 0);
-        //}
-        //if (Input.GetKey(KeyCode.RightArrow))
-        //{
-        //    transform.Translate(0.1f, 0, 0);
-        //}
-        //if (Input.GetButton("Horizontal"))
-        //{
-        //    transform.Translate(0.1f, 0, 0);
-        //}
+    [SerializeField]
+    private float speed = 0.2f;
 
+    [SerializeField]
+    private float jumpHight = 6;
+
+    private Rigidbody2D myRigidBody2D;
+
+    private bool isOnGround;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        isOnGround = true;
+
+    }
+    
+    private void MyNewFunction(Collision2D collision)
+    {
+
+
+
+    }
+
+    
+
+    // Use this for initialization
+    void Start ()
+    {
+
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+              
+	}
+
+    
+
+    // Update is called once per frame
+    void Update () {
+        
         float horizontalInput = Input.GetAxis("Horizontal");
 
         Debug.Log("horizontal input: " + horizontalInput);
 
 
 
-        float speed = 0.2f;
-
-        transform.Translate(speed * horizontalInput, 0, 0);
 
 
+        // Refactoring to use the unity physics system...
+        //transform.Translate(speed * horizontalInput, 0, 0);
 
-        if (Input.GetButtonDown("Jump"))
+        myRigidBody2D.velocity = 
+            new Vector2(speed * horizontalInput, myRigidBody2D.velocity.y);
+
+
+
+
+        if (Input.GetButtonDown("Jump") && isOnGround)
         {
-            transform.Translate(0, 6, 0);
+            myRigidBody2D.velocity = new Vector2(myRigidBody2D.velocity.x, jumpHight);
+
+            isOnGround = false;
         }
     }
+}
+
+
+public class Player
+{
+    
+    private int numberOfLives = 3;
+    private float hitPoints = 100;
+    private float money = 20;
+
+    public void TakeDamage(float amountOfDamage)
+    {
+        hitPoints -= amountOfDamage;
+    }
+
+}
+
+
+public class Enemy
+{
+    [SerializeField]
+    float attackDamage = 5;
+
+    private void DamagePlayer(Player playerWeTouched)
+    {
+        
+        playerWeTouched.TakeDamage(attackDamage);
+    }
+
 }
