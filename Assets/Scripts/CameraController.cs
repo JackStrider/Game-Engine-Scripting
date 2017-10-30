4c5ddd84ab05aff7b88429ccf244e04e78c72f91
William Rossi <William.Rossi@iam.local>
2017-10-30 14:04:41 -0500

    Newest build
    *all 3 level tile maps
    *Charecter animations
    *Coin script
    *ambient sound object

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    Transform objectToFollow;
+
+    [SerializeField]
+    float yOffSet;
+
+    [SerializeField]
+    float xOffSet;
+
+    float zOffset;
+	// Use this for initialization
+	void Start ()
+    {
+        zOffset = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Vector3 newPosition = 
+            new Vector3(objectToFollow.position.x + xOffSet, objectToFollow.position.y +yOffSet, zOffset);
+        transform.position = newPosition;
+	}
+}
